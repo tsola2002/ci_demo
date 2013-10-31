5ceb8eee2d46e8e76a1c56aec4d5e6e8f2b435fd
omatsola sobotie <tsola2002@yahoo.co.uk>
2013-10-31 17:43:26 +0000

    calendar template for structure added & calendar has been styled to look nicer

--- application/models/my_cal_model.php
+++ application/models/my_cal_model.php
@@ -11,16 +11,72 @@
 
 class My_cal_model extends CI_Model {
 
-    function generate($year, $month){
-        $conf = array(
+    var $conf;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->conf = array(
             'start_day' => 'monday',
             'show_next_prev' => true,
             'next_prev_url' => base_url() .'my_cal/display'
         );
 
+        $this->conf['template'] = '
+
+              {table_open}<table class="calendar" border="0" cellpadding="0" cellspacing="0">{/table_open}
+
+       {heading_row_start}<tr>{/heading_row_start}
+
+       {heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
+       {heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+       {heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
+
+       {heading_row_end}</tr>{/heading_row_end}
+
+       {week_row_start}<tr>{/week_row_start}
+       {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+       {week_row_end}</tr>{/week_row_end}
+
+       {cal_row_start}<tr class="days">{/cal_row_start}
+       {cal_cell_start}<td>{/cal_cell_start}
+
+       {cal_cell_content}
+        <div class="day_num">{day}</div>
+        <div class="content">{content}</div>
+       {/cal_cell_content}
+       {cal_cell_content_today}
+            <div class="day_num highlight">{day}</div>
+            <div class="content">{content}</div>
+       {/cal_cell_content_today}
+
+       {cal_cell_no_content}
+            <div class="day_num">{day}</div>
+       {/cal_cell_no_content}
+       {cal_cell_no_content_today}
+            <div class="day_num highlight">{day}</div>
+       {/cal_cell_no_content_today}
+
+       {cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+       {cal_cell_end}</td>{/cal_cell_end}
+       {cal_row_end}</tr>{/cal_row_end}
+
+       {table_close}</table>{/table_close}
+
+
+        ';
+
+    }
+
+
+    function generate($year, $month){
+
+
 
         // echo "hello frm cal";
-        $this->load->library('calendar', $conf);
+        $this->load->library('calendar', $this->conf);
 
         $cal_data = array(
             15 => 'foo',
--- application/views/my_cal.php
+++ application/views/my_cal.php
@@ -15,6 +15,34 @@
 <head>
     <meta charset="UTF-8">
     <title>My Calendar</title>
+    <style type="text/css">
+        .calendar{
+            font-family: Arial;
+            font-size: 12px;
+        }
+
+        table.calendar{
+            border-collapse: collapse;
+            margin: auto;
+        }
+
+        .calendar .days td {
+            width: 80px;
+            height: 80px;
+            padding: 4px;
+            border: 1px solid #999;
+            vertical-align: top;
+            background-color: #def;
+        }
+
+        .calendar .days td:hover {
+            background-color: #fff;
+        }
+        .calendar .highlight {
+            font-weight: bold;
+            color: #00f;
+        }
+    </style>
 </head>
 <body>
 <?php echo $calendar; ?>
