c5b07bf3a65faf30250a6d94af1140b07a53a30f
omatsola sobotie <tsola2002@yahoo.co.uk>
2013-11-02 09:29:01 +0000

    adding access to codeigniter's  super object for  standalone llibrary to have access other libraries functionality

--- application/libraries/Foo.php
+++ application/libraries/Foo.php
@@ -11,11 +11,28 @@
 
 class Foo {
 
+    var $CI;
+
+    public function __construct()
+    {
+
+        //returns instance of super object which has to be referenced instead of being cloned or copied
+        // =& references instead of making a copy
+        $this->CI =& get_instance();
+        echo "constructor was called. <br />";
+    }
+
     //creating a custom library
     //when you create a new library the first character must be capitalized
 
     function test($value){
         echo "you passed me $value. <br />";
+
+        //CI now contains loader library which can now be used
+        $this->CI->load->library('config');
+
+        //now that we have access to super object we can now config library
+        echo "You language is: ". $this->CI->config->item('language');
     }
 
 }
